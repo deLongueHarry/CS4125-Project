fb20490e8a9a1a5f6aa580e33bc5af7f7c2c5f08
awphutt97 <37743355+awphutt97@users.noreply.github.com>
2018-12-01 10:22:16 +0000

    Update Store.java

--- cs4125/src/other/Store.java
+++ cs4125/src/other/Store.java
@@ -1,4 +1,4 @@
-package other;
+package cs4125;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -17,29 +17,29 @@ import transactions.*;
 
 public class Store {
 	
-	public static ArrayList<Employee> employees = new ArrayList<>();
-	public static ArrayList<Customer> customers = new ArrayList<>();
-	public static ArrayList<Product> products = new ArrayList<>();
-	public static ArrayList<Order> orders = new ArrayList<>();
-	public static ArrayList<StockItem> stockItems = new ArrayList<>();
-	public static ArrayList<OrderItem> orderItems = new ArrayList<>();
-	public static ArrayList<Sales> sales = new ArrayList<>();
-	public static ArrayList<Returns> returnsList = new ArrayList<>();
-	public static Account ac;
-	
+	public static ArrayList<Employee> employees = new ArrayList<Employee>();
+	public static ArrayList<Customer> customers = new ArrayList<Customer>();
+	public static ArrayList<Product> products = new ArrayList<Product>();
+	public static ArrayList<Order> orders = new ArrayList<Order>();
+	public static ArrayList<StockItem> stockItems = new ArrayList<StockItem>();
+	public static ArrayList<OrderItem> orderItems = new ArrayList<OrderItem>();
+	public static ArrayList<Transactions> sales = new ArrayList<>();
+	public static ArrayList<Transactions> returnsList = new ArrayList<>();
+	public static Account ac = new Account(0);
+		
 	public static void main(String[] args) throws IOException {
-				
 		
 		loadFromArrayLists();
 		checkStockLevels();
-
+		
+		
 		System.out.println("- Login info -\nFor testing stockEmp code: ID, Password = 12");
 		System.out.println("For testing manager code:  ID, Password = 13\n");
 		
 		UI store = new StoreUI();
 		store.startInterface();
 		
-		writeToFiles();
+		//writeToFiles();
 	}
 	
 	public static void loadFromArrayLists() throws FileNotFoundException, IOException {
@@ -70,21 +70,66 @@ public class Store {
 			employees.add(tempEmp);
 		}
 		empScanner.close();
+				
+		File custFile = new File("customers.txt");
+		Scanner custScanner = new Scanner(custFile);
+		ArrayList<String> listOfAllergens = new ArrayList<>();
+
+		while(custScanner.hasNext()) {
+			
+			String[] custStr = custScanner.nextLine().split(",");
+			String[] tempAll = custStr[3].split("/");
+			
+			for (int i = 0; i < tempAll.length; i++)
+			{
+				listOfAllergens.add(tempAll[i]);
+			}
+			if(custStr.length > 5)
+			{
+				ArrayList<Voucher> vouchers = new ArrayList<>();
+				String[] tempVouchers = custStr[5].split("/");
+				Voucher tempVouch;
+				for (int i = 0; i < tempVouchers.length; i+= 2)
+				{
+					tempVouch = new Voucher(Integer.parseInt(tempVouchers[i]), Double.parseDouble(tempVouchers[i+1]));
+					vouchers.add(tempVouch);
+				}
+				Customer tempCust = new Customer(Integer.parseInt(custStr[0]), custStr[1], Integer.parseInt(custStr[2]), listOfAllergens, custStr[4], vouchers);
+				customers.add(tempCust);
+			}
+			else
+			{
+				ArrayList<Voucher> empty = new ArrayList<>();
+				Customer tempCust = new Customer(Integer.parseInt(custStr[0]), custStr[1], Integer.parseInt(custStr[2]), listOfAllergens, custStr[4], empty);
+				customers.add(tempCust);
+			}
+		}
+		custScanner.close();
+		
+		
 		
 		File prodFile = new File("products.txt");
 		Scanner prodScanner = new Scanner(prodFile);	
+		
+		ArrayList<String> allergenList = new ArrayList<String>();
 		while(prodScanner.hasNext()) {
 			
 			String[] prodStr = prodScanner.nextLine().split(",");
-			ArrayList<String> allergens = new ArrayList<>();
-			for (int i = 0; i < prodStr.length - 5; i++)	{
-				allergens.add(prodStr[i + 5]);
+			String tempAl = prodStr[6].trim();
+			String[] allergens = tempAl.split("/");
+			
+			for (int i = 0; i < allergens.length; i++) {
+					allergenList.add(allergens[i]);
 			}
-			Product tempProd = new Product(Integer.parseInt(prodStr[0]), Integer.parseInt(prodStr[1]), Double.parseDouble(prodStr[2]), prodStr[3], prodStr[4], prodStr[5], allergens);
+			
+			
+			Product tempProd = new Product(Integer.parseInt(prodStr[0]), prodStr[1], prodStr[2], 
+											Double.parseDouble(prodStr[3]), prodStr[4], 
+												Integer.parseInt(prodStr[5]), allergenList);
 			
 			products.add(tempProd);
 		}
-		prodScanner.close();		
+		prodScanner.close();
 		
 		Product temp = null;	
 		File stockItmFile = new File("stockItems.txt");
@@ -99,12 +144,11 @@ public class Store {
 					temp = products.get(i);
 				}
 			}
-			StockItem tempStockItm = new StockItem(Integer.parseInt(stockItmStr[0]), temp, Integer.parseInt(stockItmStr[2]), Double.parseDouble(stockItmStr[3]), stockItmStr[4]);
+			//int stockItmID, Product prod, int qty, String useBy
+			StockItem tempStockItm = new StockItem(Integer.parseInt(stockItmStr[0]), temp, Integer.parseInt(stockItmStr[2]), stockItmStr[3]);
 			stockItems.add(tempStockItm);
 		}
-		stockItmScanner.close();
-		
-		
+		stockItmScanner.close();	
 		
 		File orderItmFile = new File("orderItems.txt");
 		Scanner orderItmScanner = new Scanner(orderItmFile);
@@ -118,10 +162,44 @@ public class Store {
 					temp = products.get(i);
 				}
 			}
-			OrderItem tempOrderItm = new OrderItem(Integer.parseInt(orderItmStr[0]), temp, Integer.parseInt(orderItmStr[2]), Double.parseDouble(orderItmStr[3]));
+			OrderItem tempOrderItm = new OrderItem(Integer.parseInt(orderItmStr[0]), temp, Integer.parseInt(orderItmStr[2]));
 			orderItems.add(tempOrderItm);
 		}
 		orderItmScanner.close();
+		
+		File orderFile = new File("orders.txt");
+		Scanner orderScanner = new Scanner(orderFile);
+		
+		while(orderScanner.hasNext()) {
+			String[] orderStr = orderScanner.nextLine().split(",");
+			
+
+			boolean paid;
+			ArrayList<OrderItem> items = new ArrayList<>();
+			
+			String[] orderItemsInfo = orderStr[1].split("/");
+			for (int i = 0; i < orderItemsInfo.length; i++)	{
+				for (int j = 0; j < orders.size(); j++)	{
+					if (Integer.parseInt(orderItemsInfo[i]) == orderItems.get(j).getItmID())	{
+						items.add(orderItems.get(j));
+					}
+				}
+			}
+			
+			if (orderStr[4].toLowerCase() == "true")	{
+				paid = true;
+			}
+			else	{
+				paid = false;
+			}
+			for (int i = 0; i < employees.size(); i++)	{
+				if (Integer.parseInt(orderStr[3]) == employees.get(i).getID())	{
+					Order tempOrd = new Order(Integer.parseInt(orderStr[0]), items, orderStr[2], employees.get(i), paid);
+					orders.add(tempOrd);
+				}
+			}
+		}
+		orderScanner.close();
 	}
 	
 	public static void checkStockLevels() {
@@ -132,15 +210,17 @@ public class Store {
 			
 			Product currentProd = stockItems.get(i).getProduct();
 			if (stockItems.get(i).getQty() < threshold) {
-				OrderItem currentItem = new OrderItem((i+1), currentProd, 4, (4*currentProd.getCostPrice()));
+				
+				OrderItem currentItem = new OrderItem((i+1), currentProd, 4);
 				orderItems.add(currentItem);
 			}
 		}
 		orders.add(todaysOrder);
 	}
+	
 	//Writes ArrayList Data to files
-	//Author: Alex
-	public static void writeToFiles() throws IOException{
+	//Author: Alex	
+	public static void writeToFiles() throws IOException, FileNotFoundException{
 		
 		//Writing employees to file
 		File emp = new File("employees.txt");
@@ -164,9 +244,7 @@ public class Store {
 		i = 0;
 		for (; i < customers.size(); i++)	{
 			eachLine += Integer.toString(customers.get(i).getCustID()) + ",";
-			for (; j < customers.get(i).getCreditCards().size(); j++)	{
-				eachLine += Integer.toString(customers.get(i).getCreditCards().get(j)) + ",";
-			}
+			eachLine += customers.get(i).getCreditCard() + ",";
 			j = 0;
 			for (; j < customers.get(i).getAllergens().size(); j++)	{
 				eachLine += customers.get(i).getAllergens().get(j) + ",";
@@ -194,8 +272,15 @@ public class Store {
 			eachLine += products.get(i).getType() + ",";
 			eachLine += products.get(i).getCompany() + ",";
 			
-			for (; j < products.get(i).getAllergens().size(); j++)	{
-				eachLine += products.get(i).getAllergens().get(j) + ",";
+			if (!(products.get(i).getAllergens().size() > 0))
+			{
+				for (; j < products.get(i).getAllergens().size(); j++)	{
+					eachLine += products.get(i).getAllergens().get(j) + ",";
+				}
+			}
+			else
+			{
+				eachLine += "na/,";
 			}
 			j = 0;
 		}
@@ -211,7 +296,7 @@ public class Store {
 			eachLine += Integer.toString(orders.get(i).getOrderID()) + ",";
 			
 			for (; j < orders.get(i).getOrder().size(); j++)	{
-				eachLine += Integer.toString(orders.get(i).getOrder().get(j).getOrderItmID()) + ",";
+				eachLine += Integer.toString(orders.get(i).getOrder().get(j).getItmID()) + ",";
 			}
 			j = 0;
 			eachLine += orders.get(i).getDateOrdered() + ",";
@@ -231,7 +316,7 @@ public class Store {
 		i = 0;
 		
 		for (; i < stockItems.size(); i++)	{
-			eachLine += Integer.toString(stockItems.get(i).getStockItmID()) + ",";
+			eachLine += Integer.toString(stockItems.get(i).getItmID()) + ",";
 			eachLine += Integer.toString(stockItems.get(i).getProduct().getProductID()) + ",";
 			eachLine += Integer.toString(stockItems.get(i).getQty()) + ",";
 			eachLine += Double.toString(stockItems.get(i).getPrice()) + ",";
@@ -248,9 +333,9 @@ public class Store {
 		i = 0;
 		
 		for (; i < orderItems.size(); i++)	{
-			eachLine += Integer.toString(orderItems.get(i).getOrderItmID()) + ",";
+			eachLine += Integer.toString(orderItems.get(i).getItmID()) + ",";
 			eachLine += Integer.toString(orderItems.get(i).getProduct().getProductID()) + ",";
-			eachLine += Integer.toString(orderItems.get(i).getqty()) + ",";
+			eachLine += Integer.toString(orderItems.get(i).getQty()) + ",";
 			eachLine += Double.toString(orderItems.get(i).getPrice()) + ",";
 		}
 		BufferedWriter ordItWriter = new BufferedWriter(new FileWriter(orderItem));
@@ -267,10 +352,10 @@ public class Store {
 			eachLine += Integer.toString(sales.get(i).getTransID()) + ",";
 			eachLine += Double.toString(sales.get(i).getAmount()) + ",";
 			for (; j < sales.get(i).getItems().size(); j++)	{
-				eachLine += Integer.toString(sales.get(i).getItems().get(j).getStockItmID()) + ",";
+				eachLine += Integer.toString(sales.get(i).getItems().get(j).getItmID()) + ",";
 			}
 			eachLine += Integer.toString(sales.get(i).getCustID()) + ",";
-			eachLine += Integer.toString(sales.get(i).getCardNumb()) + ",";
+			eachLine += sales.get(i).getCardNumb() + ",";
 		}
 		BufferedWriter saleWriter = new BufferedWriter(new FileWriter(sale));
 		saleWriter.write(eachLine);
@@ -285,10 +370,10 @@ public class Store {
 			eachLine += Integer.toString(returnsList.get(i).getTransID()) + ",";
 			eachLine += Double.toString(returnsList.get(i).getAmount()) + ",";
 			for (; j < returnsList.get(i).getItems().size(); j++)	{
-				eachLine += Integer.toString(returnsList.get(i).getItems().get(j).getStockItmID()) + ",";
+				eachLine += Integer.toString(returnsList.get(i).getItems().get(j).getItmID()) + ",";
 			}
 			eachLine += Integer.toString(returnsList.get(i).getCustID()) + ",";
-			eachLine += Integer.toString(returnsList.get(i).getCardNumb()) + ",";
+			eachLine += returnsList.get(i).getCardNumb() + ",";
 		}
 		BufferedWriter returnsWriter = new BufferedWriter(new FileWriter(returns));
 		returnsWriter.write(eachLine);
@@ -301,4 +386,5 @@ public class Store {
 		accountWriter.write("" + ac.getAmount());
 		accountWriter.close();
 	}
+
 }
